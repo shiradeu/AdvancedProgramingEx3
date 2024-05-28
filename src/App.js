b97703593b7fc20d3009e76a074eefab6775d1f0
shiradeu <96350949+shiradeu@users.noreply.github.com>
2024-05-28 16:50:28 +0300

    change data that shown in page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EX3 IN PROGRESS!!
         </a>
       </header>
     </div>
